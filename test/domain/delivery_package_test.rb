5cb5e5f34422b1798934dbde8b9554c1a8e0ed45
Eduardo Coronel <m.coronel.seg@gmail.com>
2019-12-02 17:30:50 -0600

    UI

--- test/domain/delivery_package_test.rb
+++ test/domain/delivery_package_test.rb
@@ -120,4 +120,72 @@ class DeliveryPackageTest < ActiveSupport::TestCase
     
     assert_equal 3.79, delivery.calculate_real_total_weight.round(2)
   end
+
+  test "should calculate return correct overweight data when the package has overweight" do
+    tracking_number = "149331877648230"
+    carrier = "Fedex"
+    parcel = Hash[
+      "length", 30,
+      "width", 25,
+      "height", 10,
+      "weight", 1,
+      "distance_unit", "CM",
+      "mass_unit", "KG"
+    ]
+    real_parcel = Hash[
+      "length", 58,
+      "width", 30,
+      "height", 12,
+      "weight", 1,
+      "distance_unit", "CM",
+      "mass_unit", "KG"
+    ]
+
+    delivery = Domain::DeliveryPackage.new(
+      tracking_number,
+      carrier,
+      parcel,
+      real_parcel,
+    )
+
+    overweight_data = delivery.get_overweight_data
+
+    assert overweight_data[:has_overweight]
+    assert_equal 2, overweight_data[:label_weight]
+    assert_equal 3, overweight_data[:exceeding_weight]
+  end
+
+  test "should calculate return correct overweight data when the package has" do
+    tracking_number = "149331877648230"
+    carrier = "Fedex"
+    parcel = Hash[
+      "length", 30,
+      "width", 25,
+      "height", 10,
+      "weight", 1,
+      "distance_unit", "CM",
+      "mass_unit", "KG"
+    ]
+    real_parcel = Hash[
+      "length", 30,
+      "width", 25,
+      "height", 10,
+      "weight", 1,
+      "distance_unit", "CM",
+      "mass_unit", "KG"
+    ]
+
+    delivery = Domain::DeliveryPackage.new(
+      tracking_number,
+      carrier,
+      parcel,
+      real_parcel,
+    )
+
+    overweight_data = delivery.get_overweight_data
+    puts overweight_data
+
+    assert_not overweight_data[:has_overweight]
+    assert_equal 0, overweight_data[:exceeding_weight]
+  end
 end
